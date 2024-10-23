22178b24eb8dcec85c5e87395b007890330135cc
UncleH25 <oneilljarryd@gmail.com>
2024-10-23 04:39:02 +0200

    Declared the rest of the pages and variable css file
    Declared the rest of the pages for the web application, and declared the variables CSS file, which will handle redundant CSS, such background colours, button colours, etc.

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SplashPage from './pages/splashPage';
 import LoginPage from './pages/loginPage';
 import RegisterPage from './pages/registerPage';
 import HomePage from './pages/homePage'; 
 import NotAuthorizedPage from './pages/notAuthorizedPage';
-import ProtectedRoute from './components/auth/protectedRoute';
+import DashboardPage from './components/auth/dashboardPage';
+import SpeciesSearchPage from './pages/speciesSearchPage';
+import SpeciesProfilePage from './pages/speciesProfilePage';
+import UserPreferencesPage from './pages/userPreferencesPage';
 import AdminDashboard from './components/auth/adminDashboard';
+import ProtectedRoute from './components/auth/protectedRoute';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<SplashPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/home" element={<HomePage />} />  {/* Home route for regular users */}
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/species-search" element={<SpeciesSearchPage />} />
+        <Route path="/species-profile" element={<SpeciesProfilePage />} />
+        <Route path="/user-settings" element={<UserPreferencesPage />} />
         <Route path="/not-authorized" element={<NotAuthorizedPage />} />
         
         {/* Protect the Admin route */}
